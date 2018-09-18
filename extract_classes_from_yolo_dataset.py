4a481271749c04915dbd2f046ec1f9f9bd2c813c
Hisashi KOMINE <hssh@solo.local>
2018-09-19 02:57:08 +0900

    Migrarion to 3.5

--- extract_classes_from_yolo_dataset.py
+++ extract_classes_from_yolo_dataset.py
@@ -18,7 +18,7 @@ def main(args):
     labels_dir = os.path.join(args.output_dir, 'labels')
     os.makedirs(labels_dir, exist_ok=True)
 
-    logger.info(f'Extract classes {args.extract_class_nos} from {args.images_list}')
+    logger.info('Extract classes {} from {}'.format(args.extract_class_nos, args.images_list))
     with open(args.images_list, 'r') as f:
         for image_path in f.readlines():
             image_path = image_path.rstrip('\n')
@@ -35,15 +35,15 @@ def main(args):
                         labels.append([args.extract_class_nos.index(int(row[0])), *row[1:5]])
 
             if labels:
-                logger.info(f'Found labels on {os.path.basename(image_path)}')
+                logger.info('Found labels on {}'.format(os.path.basename(image_path)))
 
                 image_dst_path = os.path.join(images_dir, os.path.basename(image_path))
                 label_dst_path = os.path.join(labels_dir, os.path.basename(label_path))
 
-                logger.info(f'Copy image {image_path} -> {image_dst_path}')
+                logger.info('Copy image {} -> {}'.format(image_path, image_dst_path))
                 shutil.copyfile(image_path, image_dst_path)
 
-                logger.info(f'Write labels -> {label_path}')
+                logger.info('Write labels -> {}'.format(label_path))
                 with open(label_dst_path, 'w') as _f:
                     writer = csv.writer(_f, delimiter=" ")
                     writer.writerows(labels)
