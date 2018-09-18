efd265f995e824bc2ffbaa8b940fdda8b6e215f2
Hisashi KOMINE <hssh@solo.local>
2018-09-19 02:56:47 +0900

    Remove comment

--- import_dataset.py
+++ import_dataset.py
@@ -69,67 +69,6 @@ def main(args):
                 for row in reader:
                     c.execute(*sql.insert_into_images(ref["table"], row))
 
-        # classes_tables = [
-        #     ('classes', data.CLASSES_CSV),
-        # ]
-        # for table, data_csv in classes_tables:
-        #     logger.info(f'Creating table {table}')
-        #     c.executescript(sql.create_table_classes(table))
-        #
-        #     logger.info(f'Importing classes from {data_csv} to {table}')
-        #     with open(data_csv, 'r') as f:
-        #         reader = csv.reader(f)
-        #         for row in reader:
-        #             c.execute(*sql.insert_into_classes(table, row))
-
-        # boxes_tables = [
-        #     (data.train.BOXES_TABLE, data.train.BOXES_CSV),
-        #     (data.validation.BOXES_TABLE, data.validation.BOXES_CSV),
-        #     (data.test.BOXES_TABLE, data.test.BOXES_CSV),
-        # ]
-        # for table, data_csv in boxes_tables:
-        #     logger.info(f'Creating table {table}')
-        #     c.executescript(sql.create_table_bboxes(table))
-        #
-        #     logger.info(f'Importing boxes from {data_csv} to {table}')
-        #     with open(data_csv, 'r') as f:
-        #         reader = csv.reader(f)
-        #         next(reader)
-        #         for row in reader:
-        #             c.execute(*sql.insert_into_bboxes(table, row))
-        #
-        # labels_tables = [
-        #     (data.train.LABELS_TABLE, data.train.LABELS_CSV),
-        #     (data.validation.LABELS_TABLE, data.validation.LABELS_CSV),
-        #     (data.test.LABELS_TABLE, data.test.LABELS_CSV),
-        # ]
-        # for table, data_csv in labels_tables:
-        #     logger.info(f'Creating table {table}')
-        #     c.executescript(sql.create_table_labels(table))
-        #
-        #     logger.info(f'Importing labels from {data_csv} to {table}')
-        #     with open(data_csv, 'r') as f:
-        #         reader = csv.reader(f)
-        #         next(reader)
-        #         for row in reader:
-        #             c.execute(*sql.insert_into_labels(table, row))
-        #
-        # images_tables = [
-        #     (data.train.IMAGES_TABLE, data.train.IMAGES_CSV),
-        #     (data.validation.IMAGES_TABLE, data.validation.IMAGES_CSV),
-        #     (data.test.IMAGES_TABLE, data.test.IMAGES_CSV),
-        # ]
-        # for table, data_csv in images_tables:
-        #     logger.info(f'Creating table {table}')
-        #     c.executescript(sql.create_table_images(table))
-        #
-        #     logger.info(f'Importing images from {data_csv} to {table}')
-        #     with open(data_csv, 'r') as f:
-        #         reader = csv.reader(f)
-        #         next(reader)
-        #         for row in reader:
-        #             c.execute(*sql.insert_into_images(table, row))
-
         conn.commit()
 
 
